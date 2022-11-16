8576ab252010740a20de0876a2225cccc55ec1a7
Josh Hancock <livingicon@gmail.com>
2022-11-15 20:31:20 -0500

    Add 'ship is sunk' test to ship.test.js

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,21 +1,17 @@
 import Ship from '../ship.js';
-import gunboat from '../index.js';
 
 // SHIP TESTS (only test methods and properties used outside of ship object)
 
-test("ship created", () => {
+test('ship created', () => {
   const newShip = new Ship(3, 2);
   expect(newShip).toEqual({ length: 3, hits: 2 });
 });
 
-
-
-// test('Create ship', () => {
-
-// });
-
-
-
-// test('Ship is sunk', () => { //working (requires gunboat though)
+// test('each hit adds to hits total', () => {
 //   expect(gunboat.isSunk()).toBe(false);
 // });
+
+test('ship is sunk', () => {
+  const newShip = new Ship(3, 3)
+  expect(newShip.isSunk()).toBe(true);
+});
